e52928af50f39efc9e754b1e9e73451dc0b4ff45
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-20 10:46:04 +0100

    fav

--- resources/views/front/includes/top-head.blade.php
+++ resources/views/front/includes/top-head.blade.php
@@ -27,14 +27,14 @@
     <link rel="stylesheet" href="{{asset('front/theme-luiza/styles/style.min.css')}}"/>
     <link rel="stylesheet" href="{{asset('front/theme-luiza/theme-custom.css')}}"/>
     <link rel="shortcut icon" href="{{asset('front/theme-luiza/images/placeholders/logo.png')}}" type="image/x-icon"/>
-    <link rel="apple-touch-icon" href="{{asset('front/theme-luiza/apple-touch-icon.png')}}"/>
-    <link rel="apple-touch-icon" sizes="57x57" href="{{asset('front/theme-luiza/apple-touch-icon-57x57.png')}}"/>
-    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('front/theme-luiza/apple-touch-icon-72x72.png')}}"/>
-    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('front/theme-luiza/apple-touch-icon-76x76.png')}}"/>
-    <link rel="apple-touch-icon" sizes="114x114" href="{{asset('front/theme-luiza/apple-touch-icon-114x114.png')}}"/>
-    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('front/theme-luiza/apple-touch-icon-120x120.png')}}"/>
-    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('front/theme-luiza/apple-touch-icon-144x144.png')}}"/>
-    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('front/theme-luiza/apple-touch-icon-152x152.png')}}"/>
+    <link rel="apple-touch-icon" href="{{asset('front/theme-luiza/logo_fav.png')}}"/>
+{{--    <link rel="apple-touch-icon" href="{{asset('front/theme-luiza/apple-touch-icon-57x57.png')}}"/>--}}
+{{--    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('front/theme-luiza/apple-touch-icon-72x72.png')}}"/>--}}
+{{--    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('front/theme-luiza/apple-touch-icon-76x76.png')}}"/>--}}
+{{--    <link rel="apple-touch-icon" sizes="114x114" href="{{asset('front/theme-luiza/apple-touch-icon-114x114.png')}}"/>--}}
+{{--    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('front/theme-luiza/apple-touch-icon-120x120.png')}}"/>--}}
+{{--    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('front/theme-luiza/apple-touch-icon-144x144.png')}}"/>--}}
+{{--    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('front/theme-luiza/apple-touch-icon-152x152.png')}}"/>--}}
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-EBB030QMRB"></script>
 </head>
