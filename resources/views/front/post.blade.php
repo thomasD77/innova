ac39996660c9f8e5c8ff1ed152b67657794d0bb9
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-27 17:25:19 +0100

    test

--- resources/views/front/post.blade.php
+++ resources/views/front/post.blade.php
@@ -76,13 +76,13 @@
 
 <script type="application/ld+json">
 { "@context": "https://schema.org",
- "@type": "{{ $category->name }}",
+ "@type": "BlogPosting",
  "headline": "14 Ways Json Can Improve Your SEO",
  "alternativeHeadline": "and the women who love them",
  "image": "http://example.com/image.jpg",
  "award": "Best article ever written",
- "editor": "John Doe",
- "genre": "search engine optimization",
+ "editor": "{{ Auth::user() }}",
+ "genre": "search engine optimization", 
  "keywords": "seo sales b2b",
  "wordcount": "1120",
  "publisher": "Book Publisher Inc",
