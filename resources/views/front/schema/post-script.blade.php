afac5cf666d3fc01fc15c3f8e6f606f9126a0c95
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-29 21:03:27 +0100

    ja

--- resources/views/front/schema/post-script.blade.php
+++ resources/views/front/schema/post-script.blade.php
@@ -1,22 +1,20 @@
-<script type="application/ld+json">
-    {
-    "@context": "https://schema.org",
+@if($post)
+    <script type="application/ld+json">
+    { "@context": "https://schema.org",
      "@type": "BlogPosting",
-     "headline": " {{ $post->title }} ",
-     "alternativeHeadline": " {{ $post->alternativeTitle ?? null }} ",
-     "image": " {{ asset('images/posts') . $post->photo->file }} ",
+     "headline": " {{ $post->titel }} ",
+     "alternativeHeadline": " {{ $post->alternativeTitle }} ",
+     "image": " {{ asset('images/posts') . $photo->file }} ",
      "editor": "{{ $company->companyName }}",
      "genre": " {{ $post->postcategory->name }}",
-     "keywords": "{{ $post->keywords ?? null }}",
-     "wordcount": " {{ $post->wordCount ?? null  }} ",
+     "keywords": "{{ $post->keywords }}",
+     "wordcount": " {{ $post->wordCount  }} ",
      "publisher": "{{ $company->companyName }}",
-     "url": " {{ $post->seo_url ?? null }} ",
+     "url": " {{ $post->url }} ",
      "datePublished": " {{ $post->book }} ",
      "dateCreated": "{{ $post->created_at }}",
      "dateModified": " {{ $post->updated_at }}",
-     "description": " {{ $post->description ?? null }} ",
-     "articleBody": "{{ $post->body }}"
-     }
-</script>
-
-
+     "description": " {{ $post->description }} ",
+     "articleBody": "{{ $post->body }}" }
+    </script>
+@endif
