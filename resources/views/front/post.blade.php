7e7ad47a869b87aca30a4d61f7e995439f3671de
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-29 20:59:43 +0100

    sdf

--- resources/views/front/post.blade.php
+++ resources/views/front/post.blade.php
@@ -20,30 +20,7 @@
                         <div style="margin-top: 15px" class="row post">
                             @if($post)
 
-                                <script type="application/ld+json">
-                                    { "@context": "https://schema.org",
-                                     "@type": "BlogPosting",
-                                     "headline": "{{ $post->title }}",
-                                     "alternativeHeadline": "{{ $post->alternativeTitle }}",
-                                     "image": "http://example.com/image.jpg",
-                                     "award": "Best article ever written",
-                                     "editor": "John Doe",
-                                     "genre": "search engine optimization",
-                                     "keywords": "seo sales b2b",
-                                     "wordcount": "1120",
-                                     "publisher": "Book Publisher Inc",
-                                     "url": "http://www.example.com",
-                                     "datePublished": "2015-09-20",
-                                     "dateCreated": "2015-09-20",
-                                     "dateModified": "2015-09-20",
-                                     "description": "We love to do stuff to help people and stuff",
-                                     "articleBody": "You can paste your entire post in here, and yes it can get really really long.",
-                                       "author": {
-                                        "@type": "Person",
-                                        "name": "Steve"
-                                      }
-                                     }
-                                </script>
+                                @include('front.schema.post-script')
 
                                 <div class="col-md-3 post-meta">
                                     <div class="post-day">{{ \Illuminate\Support\Carbon::parse($post->book)->format('d') }}</div>
@@ -99,34 +76,5 @@
     </div>
 </section>
 <!-- END: Page content -->
-@if($post)
-
-    <script type="application/ld+json">
-{ "@context": "https://schema.org",
- "@type": "BlogPosting",
- "headline": "{{ $post->title }}",
- "alternativeHeadline": "{{ $post->alternativeTitle }}",
- "image": "http://example.com/image.jpg",
- "award": "Best article ever written",
- "editor": "John Doe",
- "genre": "search engine optimization",
- "keywords": "seo sales b2b",
- "wordcount": "1120",
- "publisher": "Book Publisher Inc",
- "url": "http://www.example.com",
- "datePublished": "2015-09-20",
- "dateCreated": "2015-09-20",
- "dateModified": "2015-09-20",
- "description": "We love to do stuff to help people and stuff",
- "articleBody": "You can paste your entire post in here, and yes it can get really really long.",
-   "author": {
-    "@type": "Person",
-    "name": "Steve"
-  }
- }
-</script>
-@endif
-
-
 @endsection
 
