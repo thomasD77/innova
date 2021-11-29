6c9dd2ede7b37bd6e58338cc691a9e5d7c41b7e0
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-29 20:35:06 +0100

    fsdf

--- resources/views/front/schema/post-script.blade.php
+++ resources/views/front/schema/post-script.blade.php
@@ -1,10 +1,11 @@
 @if($post)
-<script type="application/ld+json">
+    <script type="application/ld+json">
 { "@context": "https://schema.org",
  "@type": "BlogPosting",
- "headline": "{{ $post->title }}",
- "alternativeHeadline": " {{ $post->seo_alternativeTitle }}",
- "image": "{{ asset('images/posts') . $post->photo->file }}",
+ "headline": "14 Ways Json Can Improve Your SEO",
+ "alternativeHeadline": "and the women who love them",
+ "image": "http://example.com/image.jpg",
+ "award": "Best article ever written",
  "editor": "John Doe",
  "genre": "search engine optimization",
  "keywords": "seo sales b2b",
