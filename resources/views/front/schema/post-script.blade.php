9cd7b6c64101f05f320a38c104faa711f6ea3137
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-29 20:31:16 +0100

    test

--- resources/views/front/schema/post-script.blade.php
+++ resources/views/front/schema/post-script.blade.php
@@ -2,23 +2,23 @@
 <script type="application/ld+json">
 { "@context": "https://schema.org",
  "@type": "BlogPosting",
- "headline": " {{ $post->titel }} ",
- "alternativeHeadline": " {{ $post->seo_alternativeTitle }} ",
- "image": " {{ asset('images/posts') . $photo->file }} ",
- "award": "Best article ever written",
- "genre": " {{ $post->postcategory->name }}",
- "keywords": "{{ $post->seo_keywords }}",
- "wordcount": " {{ $post->seo_wordCount  }} ",
- "publisher": "{{ $company->companyName }}",
-     "url": " {{ $post->seo_url }} ",
-     "datePublished": " {{ $post->book }} ",
-     "dateCreated": "{{ $post->created_at }}",
-     "dateModified": " {{ $post->updated_at }}",
-     "description": " {{ $post->seo_description }} ",
- "articleBody": "{!! $post->body !!} " ,
+ "headline": "{{ $post->title }}",
+ "alternativeHeadline": " {{ $post->seo_alternativeTitle }}",
+ "image": "{{ asset('images/posts' . $post->photo->file) }}",
+ "editor": "John Doe", 
+ "genre": "search engine optimization",
+ "keywords": "seo sales b2b",
+ "wordcount": "1120",
+ "publisher": "Book Publisher Inc",
+ "url": "http://www.example.com",
+ "datePublished": "2015-09-20",
+ "dateCreated": "2015-09-20",
+ "dateModified": "2015-09-20",
+ "description": "We love to do stuff to help people and stuff",
+ "articleBody": "You can paste your entire post in here, and yes it can get really really long.",
    "author": {
     "@type": "Person",
-    "name": "Innova"
+    "name": "Steve"
   }
  }
 </script>
