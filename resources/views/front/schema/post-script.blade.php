27905a5263694933eb315aa4822eac3e91d18974
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-30 08:11:43 +0100

    live

--- resources/views/front/schema/post-script.blade.php
+++ resources/views/front/schema/post-script.blade.php
@@ -12,14 +12,14 @@
      "wordcount": "{{ $post->wordCount }}",
      "publisher": "{{ $company->companyName }}",
      "url": "{{ $post->url }}",
-     "datePublished": "2015-09-20",
-     "dateCreated": "2015-09-20",
-     "dateModified": "2015-09-20",
-     "description": "We love to do stuff to help people and stuff",
-     "articleBody": "You can paste your entire post in here, and yes it can get really really long.",
+     "datePublished": "{{ $post->book }}",
+     "dateCreated": "{{ $post->created_at }}",
+     "dateModified": "{{ $post->updated_at }}",
+     "description": "{{ $post->description }}",
+     "articleBody": "{{ $post->body }}",
        "author": {
-        "@type": "Person",
-        "name": "Steve"
+        "@type": "Company",
+        "name": "{{ $company->companyName }}"
       }
      }
 </script>
