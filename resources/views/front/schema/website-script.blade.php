1c4b07b8d127e1c895824adb9a989809c62f6196
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-12-17 17:24:59 +0100

    fix scripts schema

--- resources/views/front/schema/website-script.blade.php
+++ resources/views/front/schema/website-script.blade.php
@@ -1,12 +1,14 @@
-@if ( $company )
-    @if( $account == 'active' )
-        <script type="application/ld+json">
-            {
-            "@context": "http://schema.org",
-            "@type": "WebSite",
-              "name": "{{ $company->companyName }}",
-              "url": "https://{{ $company->url }}"
-            }
-        </script>
+@if ( isset($company) )
+    @if( isset($account))
+        @if( $account == 'active')
+            <script type="application/ld+json">
+                {
+                "@context": "http://schema.org",
+                "@type": "WebSite",
+                  "name": "{{ $company->companyName }}",
+                  "url": "https://{{ $company->url }}"
+                }
+            </script>
+        @endif
     @endif
 @endif
